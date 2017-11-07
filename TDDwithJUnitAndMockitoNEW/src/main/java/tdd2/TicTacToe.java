16beb9201819b3a5f5935f70f83ab09b0bf58ee0
unknown <frank_bartnitzek@web.de>
2017-11-07 14:19:34 +0100

    tdd: updated impl for requirement 1

--- TDDwithJUnitAndMockitoNEW/src/main/java/tdd2/TicTacToe.java
+++ TDDwithJUnitAndMockitoNEW/src/main/java/tdd2/TicTacToe.java
@@ -3,11 +3,17 @@ package tdd2;
 public class TicTacToe {
 
 	private static final int SIZE = 3;
+	private char [][] board =  {{'\0', '\0', '\0'}, // 00, 01, 02
+								{'\0', '\0', '\0'}, // 10, 11, 12
+								{'\0', '\0', '\0'}};// 20, 21, 22
 
 	public void play(int x, int y) {
 		checkAxis(x);
 		checkAxis(y);
-
+		if (board[x-1][x-y] != '\0') {
+			throw new RuntimeException();
+		}
+		board[x-1][y-1] = 'X';
 	}
 
 	private void checkAxis(int x) {
