2faaad95a62adfe14eaba9d4f869443f9f2dd884
unknown <frank_bartnitzek@web.de>
2017-11-07 12:18:42 +0100

    tdd: updated implementation to match tests

--- TDDwithJUnitAndMockitoNEW/src/main/java/tdd1/FootballTeam.java
+++ TDDwithJUnitAndMockitoNEW/src/main/java/tdd1/FootballTeam.java
@@ -4,6 +4,9 @@ public class FootballTeam {
 	private int gamesWon;
 
 	public FootballTeam(int gamesWon) {
+		if (gamesWon<0) {
+			throw new IllegalArgumentException("must not be negative");
+		}
 		this.gamesWon = gamesWon;
 	}
 
