d9693402fa72d0d10bbaa56057c4fee9cf6c454b
unknown <frank_bartnitzek@web.de>
2017-11-07 14:09:05 +0100

    tdd: updated impl to test

--- TDDwithJUnitAndMockitoNEW/src/main/java/tdd2/TicTacToe.java
+++ TDDwithJUnitAndMockitoNEW/src/main/java/tdd2/TicTacToe.java
@@ -5,6 +5,8 @@ public class TicTacToe {
 	public void play(int x, int y) {
 		if (x>3 || x <1) {
 			throw new RuntimeException("outside of the X-axis");
+		} else if (y>3 || y<1) {
+			throw new RuntimeException("outside of the Y-axis");
 		}
 		
 	}
