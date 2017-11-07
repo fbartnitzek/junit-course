ef6df782e3da10bcac531952d7194fb75d39bdc1
unknown <frank_bartnitzek@web.de>
2017-11-07 12:34:05 +0100

    tdd: added simple (wrongly) impl

--- TDDwithJUnitAndMockitoNEW/src/main/java/tdd1/FootballTeam.java
+++ TDDwithJUnitAndMockitoNEW/src/main/java/tdd1/FootballTeam.java
@@ -16,6 +16,9 @@ public class FootballTeam implements Comparable<FootballTeam> {
 
 	@Override
 	public int compareTo(FootballTeam o) {
+		if (this.gamesWon > o.getGamesWon()) {
+			return 1;
+		}
 		return 0;
 	}
 
