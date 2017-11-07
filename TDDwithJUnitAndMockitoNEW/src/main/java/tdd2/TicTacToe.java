fe25d733831454b8bda08dce59773242ea25c5f9
unknown <frank_bartnitzek@web.de>
2017-11-07 14:12:11 +0100

    tdd: refactored impl, still working

--- TDDwithJUnitAndMockitoNEW/src/main/java/tdd2/TicTacToe.java
+++ TDDwithJUnitAndMockitoNEW/src/main/java/tdd2/TicTacToe.java
@@ -2,13 +2,18 @@ package tdd2;
 
 public class TicTacToe {
 
+	private static final int SIZE = 3;
+
 	public void play(int x, int y) {
-		if (x>3 || x <1) {
-			throw new RuntimeException("outside of the X-axis");
-		} else if (y>3 || y<1) {
-			throw new RuntimeException("outside of the Y-axis");
+		checkAxis(x);
+		checkAxis(y);
+
+	}
+
+	private void checkAxis(int x) {
+		if (x > SIZE || x < 1) {
+			throw new RuntimeException();
 		}
-		
 	}
 
 }
