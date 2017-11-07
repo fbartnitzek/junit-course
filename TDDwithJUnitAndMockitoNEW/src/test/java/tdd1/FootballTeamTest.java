a7397571647026c434e63a78bfd870d6261ca1f2
unknown <frank_bartnitzek@web.de>
2017-11-07 13:37:10 +0100

    tdd: updated match-compare-tests

--- TDDwithJUnitAndMockitoNEW/src/test/java/tdd1/FootballTeamTest.java
+++ TDDwithJUnitAndMockitoNEW/src/test/java/tdd1/FootballTeamTest.java
@@ -52,5 +52,23 @@ public class FootballTeamTest {
 		assertTrue("team with 3 games won should be greater than team with 2 games won",
 				team_3gamesWon.compareTo(team_2gamesWon) > 0);
 	}
+	
+	@Test
+	public void teamWithLessMatchesWonShouldBeLess() {
+		FootballTeam team_3gamesWon = new FootballTeam(3);
+		FootballTeam team_2gamesWon = new FootballTeam(2);
+		
+		assertTrue("team with 2 games won should be smaller than team with 2 games won",
+				team_2gamesWon.compareTo(team_3gamesWon) < 0);
+	}
+	
+	@Test
+	public void teamsWithEqualMatchesWonShouldBeEqual() {
+		FootballTeam team_3gamesWon = new FootballTeam(3);
+		FootballTeam team2_3gamesWon = new FootballTeam(3);
+		
+		assertTrue("both teams with 3 games won should be equal",
+				team_3gamesWon.compareTo(team2_3gamesWon) == 0);
+	}
 
 }
